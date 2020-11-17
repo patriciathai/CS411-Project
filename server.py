f241e622edf8035d4f5b7224a20f7dfee000afe2
Nicole Pineda <nicolecpineda@gmail.com>
2020-11-17 04:14:02 +0000

    Search feature done

--- server.py
+++ server.py
@@ -284,7 +284,7 @@ def customer_new_orders(cid, my_filter):
       rids.append(results['rid'])
     cursor6.close()
   elif (my_filter == 'search'):
-    name = "'%" + request.args.get('searchbox') + "%'"
+    name = "'%%" + request.args.get('searchbox') + "%%'"
     cursor7 = g.conn.execute("SELECT R.rid FROM located_in L, restaurant R WHERE R.r_name ILIKE {name} AND R.rid = L.rid AND (L.zip={string_zip} OR L.zip={string_forward_zip} OR L.zip={string_forward_again})".format(string_zip=string_zip , string_forward_zip=string_forward_zip , string_forward_again=string_forward_again, name=name))
     rids = []
     for results in cursor7:
